71dc3cef4ab98a92d8ec46dbbaa6854c7e7d8829
Jian Li <pyguni@gmail.com>
2025-04-09 08:18:11 +0000

    Feat: support multi-arch build

---
+++ multiarch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -ex
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+TAG=${TAG:-v1.4.0}
+REPO=${REPO:-quay.io/edgestack}
+PUSH=${PUSH:-}
+
+# support other container tools. e.g. podman
+CONTAINER_CLI=${CONTAINER_CLI:-docker}
+CONTAINER_BUILDER=${CONTAINER_BUILDER:-"buildx build"}
+
+# If set, just building, no pushing
+if [[ -z "${DRY_RUN:-}" ]]; then
+  PUSH="--push"
+fi
+
+# supported platforms
+PLATFORMS=linux/amd64,linux/arm64
+
+# shellcheck disable=SC2086 # intended splitting of CONTAINER_BUILDER
+${CONTAINER_CLI} ${CONTAINER_BUILDER} \
+  --platform ${PLATFORMS} \
+  ${PUSH} \
+  -f ./Dockerfile \
+  -t "${REPO}"/virt-launcher:"${TAG}" .
