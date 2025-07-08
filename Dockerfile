b5ed7c53056777a6238ec4ea12293fd8e4ce83e1
Jian Li <pyguni@gmail.com>
2025-07-08 04:04:13 -0400

    Support virt-launcher v1.5.2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM quay.io/kubevirt/virt-launcher:v1.4.0
+FROM quay.io/kubevirt/virt-launcher:v1.5.2
 
 ARG TARGETARCH
 ENV ARCH=${TARGETARCH:-amd64}
--- multiarch.sh
+++ multiarch.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-TAG=${TAG:-v1.4.0}
+TAG=${TAG:-v1.5.2}
 REPO=${REPO:-quay.io/edgestack}
 PUSH=${PUSH:-}
 
